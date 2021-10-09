64a3c7e19bc3077ccb8f30131a05c1b2b2c68c55
anshutre <64204670+anshutre@users.noreply.github.com>
2021-10-09 23:32:51 +0530

    Lazy loading and suspense fallback added

--- src/App.js
+++ src/App.js
@@ -2,29 +2,26 @@ import './App.css';
 
 import React, { Suspense, lazy } from 'react';
 
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 // import layout hoc
 import Layout from './hoc/Layout';
-import HomePage from './views/HomePage/HomePage';
-import ContactPage from "./views/Contact/Contact";
+// import HomePage from './views/HomePage/HomePage';
+// import ContactPage from "./views/Contact/Contact";
 // import views
-// const HomePage = lazy(() => import('./views/HomePage/HomePage'));
-// const ContactPage = lazy(() => import('./views/Contact/Contact'));
+const HomePage = lazy(() => import('./views/HomePage/HomePage'));
+const ContactPage = lazy(() => import('./views/Contact/Contact'));
 
 function App() {
   return (
-      
-        // <Suspense fallback={<div>Loading...</div>}>
-          <Layout>
+    <Layout>
+      <Suspense fallback={<div>Loading...</div>}>
         <Switch>
-        
           <Route exact path="/" component={HomePage} />
           <Route path="/contact" component={ContactPage} />
         </Switch>
-        </Layout>
-        // </Suspense>
-      
+      </Suspense>
+    </Layout>
   );
 }
 
